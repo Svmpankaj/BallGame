290e3a64f52ff73acf71326e66a723915236f224
Svmpankaj <svmpankaj@gmail.com>
2023-03-03 19:26:20 +0530

    ball is completed

--- pipe.js
+++ pipe.js
@@ -1,20 +1,42 @@
-const HOLE_HEIGHT = 120
+const HOLE_HEIGHT = 200
+const PIPE_WIDTH = 120
 const PIPE_INTERVAL = 1500
 const PIPE_SPEED = .75
-const pipes = []
-let timeSinceLastPipe = 0
+let pipes = []
+let timeSinceLastPipe
+let passedPipeCount
+
+export function setupPipes() {
+    document.documentElement.style.setProperty("--pipe-width", PIPE_WIDTH)
+    document.documentElement.style.setProperty("--hole-height", HOLE_HEIGHT)
+    pipes.forEach(pipe => pipe.remove())
+    timeSinceLastPipe = PIPE_INTERVAL
+    passedPipeCount = 0
+}
 
 export function updatePipes(delta) {
     timeSinceLastPipe += delta
 
-    if (timeSinceLastPipe < PIPE_INTERVAL) {
+    if (timeSinceLastPipe > PIPE_INTERVAL) {
         timeSinceLastPipe -= PIPE_INTERVAL
         createPipe()
     }
 
     pipes.forEach(pipe => {
+        if (pipe.left + PIPE_WIDTH < 0) {
+            passedPipeCount++
+            return pipe.remove()
+        }
         pipe.left = pipe.left - delta * PIPE_SPEED
     })
+}
+
+export function getPassedPipesCount() {
+    return passedPipeCount
+}
+
+export function getPipeRects() {
+    return pipes.flatMap(pipe => pipe.rects())
 }
 
 function createPipe() {
@@ -32,10 +54,20 @@ function createPipe() {
     )
     const pipe = {
         get left() {
-            return parserFloat(getComputedStyle(pipeElem).getPropertyValue("--pipe-left"))
+            return parseFloat(getComputedStyle(pipeElem).getPropertyValue("--pipe-left"))
         },
         set left(value) {
             pipeElem.style.setProperty("--pipe-left", value)
+        },
+        remove() {
+            pipes = pipes.filter(p => p !== pipe)
+            pipeElem.remove
+        },
+        rects() {
+            return [
+                topElem.getBoundingClientRect(),
+                bottomElem.getBoundingClientRect(),
+            ]
         }
     }
     pipe.left = window.innerWidth
